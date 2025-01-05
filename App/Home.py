e1218e8a2837c89195b323e4d8e2b021f206511f
Juan Bispo <juan.bisposantos05@gmail.com>
2025-01-05 09:33:10 -0300

    adicionando imagens de icone, cabeçalhos, títulos e menu

---
+++ App/Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(page_title="Home", page_icon=r"C:\Users\yamim\OneDrive\Documentos\App Streamlit Projeto Predição Dias Atrasados\Imgs\Home_PageIcon1.png", layout="wide")
+
+st.title('Previsão de Dias atrasados na produção.')
+
+st.header('Bem Vindo!')
+
+coluna_texto_corpo1, coluna_texto_corpo2 = st.columns(2)
+
+coluna_texto_corpo1.write('Bem-vindo à nossa ferramenta de previsão de atraso na produção!\n'
+         'Com essa ferramenta, você pode prever o atraso na produção de um item com base em seu código, quantidade e processo de produção.\n'
+        'Basta inserir as informações do item e clicar em "Prever" para obter uma estimativa do atraso em dias.\n')
+
+
+coluna_texto_corpo2.write('Esperamos que essa ferramenta ajude você a planejar sua produção de forma mais eficiente e evitar atrasos. '    
+        'Para saber a média de dias de atraso por item, acesse a página: '
+        'Para acessar a página de predições, preencha o formulário: ')
+
+st.sidebar.title('Menu')
